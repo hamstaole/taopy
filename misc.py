f72431da697e4cfbaa51a09b45e9371a5a4ba7a1
batbien <we50ygul@gmail.com>
2019-05-07 10:39:46 +0200

    More misc tasks

--- misc.py
+++ misc.py
@@ -1,22 +1,52 @@
-# Double the given number and return the result
 def double(num):
+    """ Double the given number and return the result.
+    """
     result = num * 2
     return(result)
 
-# Convert the given input of type Integer to type String and return the result
-# Sample input/output: 
-# 0   -> "0"
-# -13 -> "-13"
-# 10  -> "10"
-def convert_to_str(num):
-    # TODO
-    return 
 
-# Sum all the items in the given list, suppose the given list consists of numbers
-# of type Integer and is not empty.
-# Sample input/output:
-# {0,1,2,3} -> 6
-# {-1,1} -> 0
+def convert_to_str(num):
+    """Convert the given input of type Integer to type String and return the result.
+    Sample input/output:
+    0   -> "0"
+    -13 -> "-13"
+    10  -> "10"
+    """
+    # TODO
+    return
+
+
 def sum_list(aList):
+    """Sum all the items in the given list, suppose the given list consists of numbers
+    of type Integer and is not empty.
+    Sample input/output:
+    {0,1,2,3} -> 6
+    {-1,1} -> 0
+    """
+    # TODO
+    return
+
+
+def multiply_list(aList):
+    """Multiply all the items in the given list, suppose the given list consists of numbers
+    of type Integer and is not empty.
+    Sample input/output:
+    {0,1,2} -> 0
+    {-1,4} -> -4
+    {2,3} -> 6
+    {-4,-5} -> 20
+    """
+    # TODO
+    return
+
+
+def find_max(aList):
+    """Return the largest number in the given list, suppose the given list consists of numbers
+    of type Integer and is not empty.
+    Sample input/output:
+    {0,1,2,3} -> 3
+    {3,0,-1,6,5} -> 6
+    {-4,-5,-3,-6} -> -3
+    """
     # TODO
     return
--- tests/test_misc.py
+++ tests/test_misc.py
@@ -4,21 +4,42 @@ import misc
 
 class TestFunctions(unittest.TestCase):
 
-   def test_double(self):
+    def test_double(self):
         self.assertEqual(misc.double(0), 0)
         self.assertEqual(misc.double(1), 2)
         self.assertEqual(misc.double(7), 14)
         self.assertEqual(misc.double(-10), -20)
 
-   def test_convert_to_str(self):
+    def test_convert_to_str(self):
         self.assertEqual(misc.convert_to_str(0), "0")
         self.assertEqual(misc.convert_to_str(1), "1")
         self.assertEqual(misc.convert_to_str(7), "7")
         self.assertEqual(misc.convert_to_str(-10), "-10")
 
-   def test_sum_list(self):
+    def test_sum_list(self):
         self.assertEqual(misc.sum_list({0}), 0)
         self.assertEqual(misc.sum_list({1}), 1)
-        self.assertEqual(misc.sum_list({1,2,3,1}), 7)
-        self.assertEqual(misc.sum_list({1,-1,10,-10,-5,-2,-3}), -10)
+        self.assertEqual(misc.sum_list({1, 2, 3, 1}), 7)
+        self.assertEqual(misc.sum_list({1, -1, 10, -10, -5, -2, -3}), -10)
 
+    def test_multiply_list(self):
+        self.assertEqual(misc.multiply_list({0}), 0)
+        self.assertEqual(misc.multiply_list({1}), 1)
+        self.assertEqual(misc.multiply_list({-1}), -1)
+        self.assertEqual(misc.multiply_list({1, 2, 3, 1}), 6)
+        self.assertEqual(misc.multiply_list({0, 4}), 0)
+        self.assertEqual(misc.multiply_list({-2, -5}), 10)
+        self.assertEqual(misc.multiply_list({-2, 5}), -10)
+        self.assertEqual(misc.multiply_list(
+            {1, -1, 10, -10, -5, -2, -3}), -3000)
+
+    def test_find_max(self):
+        self.assertEqual(misc.find_max({0}), 0)
+        self.assertEqual(misc.find_max({1}), 1)
+        self.assertEqual(misc.find_max({-1}), -1)
+        self.assertEqual(misc.find_max({1, 2, 3, 1}), 3)
+        self.assertEqual(misc.find_max({0, 4}), 4)
+        self.assertEqual(misc.find_max({-2, -5}), -2)
+        self.assertEqual(misc.find_max({-2, 5}), 5)
+        self.assertEqual(misc.find_max(
+            {1, -1, 10, -10, -5, -2, -3}), 10)
